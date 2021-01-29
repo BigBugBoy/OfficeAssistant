a2bc47eb993c68e69cb3e1ac1cc14788a982af2f
Mambar_Wong <bigbugboywang@gmail.com>
2021-01-29 20:44:58 +0800

    新建文件相关数据表

--- CreateDataBase.sql
+++ CreateDataBase.sql
@@ -1,10 +1,38 @@
 
 --*****************************创建数据库*****************************
-CREATE DATABASE [DB_wwj] ON  PRIMARY
-( NAME = 'DB_wwj', FILENAME = 'D:\04_MyDataBase\DB_wwj.mdf' , SIZE = 16384KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
-LOG ON
-( NAME = 'DB_wwj_log', FILENAME = 'D:\04_MyDataBase\DB_wwj_log.ldf' , SIZE = 6272KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
-GO
+-- CREATE DATABASE [DB_wwj] ON  PRIMARY
+-- ( NAME = 'DB_wwj', FILENAME = 'D:\04_MyDataBase\DB_wwj.mdf' , SIZE = 16384KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB )
+-- LOG ON
+-- ( NAME = 'DB_wwj_log', FILENAME = 'D:\04_MyDataBase\DB_wwj_log.ldf' , SIZE = 6272KB , MAXSIZE = 2048GB , FILEGROWTH = 10%)
+-- GO
+
+use master  --指定初始使用系统数据库
+go
+--检测名为"DB_wwj"的数据库是否存在，查询系统表
+if exists(select * from sysdatabases where name='DB_wwj')
+ drop database DB_wwj   --如果存在则删除掉
+go  --批处理结束标志
+create database DB_wwj
+on primary
+(
+  name='DB_wwj',    --数据库逻辑名
+  filename='D:\04_MyDataBase\DB_wwj.mdf',   --数据库文件名，包含路径
+  MAXSIZE =UNLIMITED    --文件不限制最大值
+  filegrowth=15%,   --文件增长率
+  size=16384KB  --初始大小
+)
+log on
+(
+  name='DB_wwj_log',    --日志文件逻辑名
+  filename='D:\04_MyDataBase\DB_wwj_log.ldf',   --日志文件物理名
+  MAXSIZE=2048GB    --日志文件最大值
+  filegrowth=5%,    --日志文件增长率
+  size=6272KB   --日志文件初始大小
+)
+go
+
+
+
 
 
 --*****************************创建表*************************
@@ -76,3 +104,34 @@ select * from Roles
 go
 
 --*******************表1系列建立完成 2021.1.24****************
+
+/*
+sql语句不能直接改变字段并设为自增，所以需要先将该字段删除，再新增该字段。
+
+alter table A  drop column [ID]--删除表A原ID列
+
+alter table A  add [ID] bigint identity(1,1)--表A新增ID列并设为自增，类型为bigint
+
+*/
+
+use DB_wwj
+create table FileClass
+(
+    fileClassID int not null,   --文件类别编号
+    fileClassName nvarchar(20), --文件类别名称
+    primary key(fileClassID)
+)
+create table FileInfo
+(
+    fileID bigint identity(1,1) not null,    --这是为长int型，步长为1，自增
+    fileName nvarchar(10000) not null,   --文件名称
+    fileDate nvarchar(12) not null,  --文件日期
+    fileClassID int not null,    --文件类别
+    fileUserID int not null, --文件拥有者ID
+    fileNote nvarchar(500),  --文件备注
+    fileDatas image not null,   --文件流数据
+    status int not null, --文件删除标识
+    --workClassID int not null,   --文件类别标识
+    primary key(fileID)
+)
+create table tb_DW
